ea30100a8a5b3ef0580aab36c1f63e135d4f982a
Luciano Scarpaci <tauxsik@gmail.com>
2023-08-22 16:28:42 -0400

    Output Panel Fix 1

--- outputpanel.py
+++ outputpanel.py
@@ -15,22 +15,35 @@ class SharedOutputPanelListener(sublime_plugin.EventListener):
 
         if markdown: output_panel.set_syntax_file("Packages/Markdown/MultiMarkdown.sublime-syntax")
 
+        # bool is false, answer_started
+        answer_started = False # Flag to keep track of when the "Answer" section starts 
+        # # Line number of the last line in the "Answer" section
+        answer_line_num = 0
+
         for line in Cacher().read_all():
             if line['role'] == 'user':
                 output_panel.run_command('append', {'characters': f'\n\n## Question\n\n'})
             elif line['role'] == 'assistant':
                 ## This one left here as there're could be loooong questions.
                 output_panel.run_command('append', {'characters': '\n\n## Answer\n\n'})
+                answer_started = True # Set the flag to indicate the "Answer" section has started 
 
             output_panel.run_command('append', {'characters': line['content']})
+            # answer started is True
+            if answer_started:
+                answer_line_num = get_number_of_lines(output_panel)
 
         output_panel.set_read_only(True)
-        num_lines = get_number_of_lines(output_panel)
-        print(f'num_lines: {num_lines}')
+        # Calculate the point to scroll to based on the line number of the last line in the "Answer" section
+        point = output_panel.text_point(answer_line_num - 1, 0)
+
+        # OLD CODE FROM ORIGINAL PLUGIN
+        # num_lines = get_number_of_lines(output_panel)
+        # print(f'num_lines: {num_lines}')
 
         ## Hardcoded to -10 lines from the end, just completely randrom number.
         ## TODO: Here's some complex scrolling logic based on the content (## Answer) required.
-        point = output_panel.text_point(num_lines - 10, 0)
+        # point = output_panel.text_point(num_lines - 10, 0)
 
         output_panel.show_at_center(point)
 
